8f8851a5b51d8def0f538519ea20b836315e25e7
whiteShtef <whiteShtef@users.noreply.github.com>
2016-09-24 17:21:36 +0200

    Added empty lines to improve readability

--- FilePath.py
+++ FilePath.py
@@ -2,7 +2,9 @@ import os
 
 # Absolute path of the python file
 path=os.path.realpath(__file__)
+
 # Absolute path of the DIR where python file is
 dir_path=os.path.dirname(path)
+
 # Name of the DIR where python file is
 dir_name=os.path.basename(dir_path)
