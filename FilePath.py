55081d8fdd9e3c6be2f72a97e594b397eab3518a
whiteShtef <whiteShtef@users.noreply.github.com>
2016-09-24 17:21:05 +0200

    Typo

--- FilePath.py
+++ FilePath.py
@@ -4,5 +4,5 @@ import os
 path=os.path.realpath(__file__)
 # Absolute path of the DIR where python file is
 dir_path=os.path.dirname(path)
- # Name of the DIR where python file is
+# Name of the DIR where python file is
 dir_name=os.path.basename(dir_path)
